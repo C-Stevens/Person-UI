a1562924ab5a047d0d1a487c08e1e55913fd9187
Colin Stevens <colin@colinjstevens.com>
2018-09-07 18:30:44 -0500

    Initial Commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Colin Stevens on 9/7/18.
+//  Copyright © 2018 Colin Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
